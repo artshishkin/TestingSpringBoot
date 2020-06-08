549c07fe38772f2faa9b7651f84cbc95138e07e8
admin <d.art.shishkin@gmail.com>
2020-06-09 00:13:13 +0300

    TDD By Example - Multiplication, Dollar Side Effects?

---
+++ src/main/java/com/artarkatesoft/Dollar.java
@@ -0,0 +1,14 @@
+package com.artarkatesoft;
+
+public class Dollar {
+    public int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+}
