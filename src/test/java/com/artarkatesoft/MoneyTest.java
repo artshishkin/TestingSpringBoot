bfd5872377800a6a9c1a827b3d350875d2893710
admin <d.art.shishkin@gmail.com>
2020-06-09 00:21:26 +0300

    TDD By Example - equals()

--- src/test/java/com/artarkatesoft/MoneyTest.java
+++ src/test/java/com/artarkatesoft/MoneyTest.java
@@ -3,6 +3,7 @@ package com.artarkatesoft;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MoneyTest {
     @Test
@@ -13,5 +14,11 @@ public class MoneyTest {
 
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+
+    @Test
+    void testEquality() {
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(5), new Dollar(10));
     }
 }
